af073e91ea29a531efb74292022951cd40bfc85b
toughasara <toughasarah@gmail.com>
2023-03-21 15:23:22 -0700

    msg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,12 +10,14 @@ void print_alphabet(void)
 
 {
 
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-        _putchar(c);
-        }
-        _putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 
 }
+
+
