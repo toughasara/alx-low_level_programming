0e46846f581b176f60f1aa6cd7a7aab768bb0740
toughasara <toughasarah@gmail.com>
2023-04-12 17:57:29 -0700

    msg

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,50 +3,52 @@
 #include <stdio.h>
 
 /**
- * argstostr - convert the params passed to the program to string
- * @ac: the argument count
- * @av: the argument vector
- *
- * Return: a pointer to a new string, or NULL if it fails
- */
+  * argstostr - convert the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
 char *argstostr(int ac, char **av)
 {
-        int ch = 0, i = 0, j = 0, k = 0;
-        char *s;
+	int ch = 0, i = 0, j = 0, k = 0;
+	char *s;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        while (i < ac)
-        {
-                while (av[i][j])
-                {
-                        ch++;
-                        j++;
-                }
+	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
 
-                j = 0;
-                i++;
-        }
+		j = 0;
+		i++;
+	}
 
-        s = malloc((sizeof(char) * ch) + ac + 1);
+	s = malloc((sizeof(char) * ch) + ac + 1);
 
-        i = 0;
-        while (av[i])
-        {
-                j = 0;
-                while (av[i][j])
-                {
-                        s[k] = av[i][j];
-                        k++;
-                        j++;
-                }
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
 
-                s[k] = '\n';
-                k++;
-                i++;
-        }
+		s[k] = '\n';
 
-        s[k] = '\0';
-        return (s);
+		j = 0;
+		k++;
+		i++;
+	}
+
+	k++;
+	s[k] = '\0';
+	return (s);
 }
